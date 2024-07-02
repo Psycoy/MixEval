03a6bff86c25760287977bcf01b95d1eab8e7856
Oliver-hpc-ai1 <olivernova1998@gmail.com>
2024-07-02 02:59:27 +0000

    fixed config

--- mix_eval/models/gemma_2_27b_instruct.py
+++ mix_eval/models/gemma_2_27b_instruct.py
@@ -1,4 +1,5 @@
 import torch
+import json
 
 from mix_eval.models.base import ChatModel
 from mix_eval.api.registry import register_model
@@ -8,7 +9,7 @@ class Gemma_2_27B_Instruct(ChatModel):
     def __init__(self, args):
         super().__init__(args)
         self.model_name = "google/gemma-2-27b-it"
-        self.attn_implementation = 'flash_attention_2' # If use default, set to None
+        self.attn_implementation = 'eager' # If use default, set to None
         self.model_dtype = torch.bfloat16
         
         self.SYSTEM_MESSAGE = None # set to None if no system message
@@ -26,3 +27,42 @@ class Gemma_2_27B_Instruct(ChatModel):
             self.model_max_len,
             self.max_input_length
         ) - self.openended_max_new_tokens
+    
+    def get_closeended_responses(self, batch, response_file):
+        formated_prompts = [d['raw_inputs']['formated_input'] for d in batch]
+        inputs = [self.apply_chat_template(self.get_messages(prompt)) for prompt in formated_prompts]
+ 
+        model_inputs = self.tokenizer(
+            inputs, 
+            return_tensors="pt",
+            padding="longest",
+            max_length=self.max_input_length_closeend,
+            truncation=True,
+            add_special_tokens=True
+            ).to(self.device)
+        
+        chunk_size = 2500
+        # print(f"model_inputs length: {model_inputs.input_ids.shape[-1]}")
+        # if model_inputs.input_ids.shape[1]<=chunk_size:
+        #     chunked = False
+        # else:
+        #     chunked = True
+        
+        chunked = False # todo remove
+        
+        # when chunked = False, it uses vanilla generate
+        responses = self.chunk_generate(
+            model_inputs,
+            self.model,
+            self.tokenizer,
+            chunk_size=chunk_size,
+            max_tokens=self.closeended_max_new_tokens,
+            chunked=chunked,
+            **self.gen_kwargs,
+            )
+        
+        with open(response_file, "a") as f:
+            for raw_dict, response in zip(batch, responses):
+                raw_dict = raw_dict['raw_inputs']
+                raw_dict['response'] = response
+                f.write(json.dumps(raw_dict) + "\n")
